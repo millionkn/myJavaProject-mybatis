e3ef806a101dfd8d5dfdfbec18cfbab1bf1c8ce6
millionkn <millionkn@sina.com>
2019-05-10 23:41:01 +0900

    ResponseBody返回默认utf8

--- src/main/java/com/mytest/controller/Main.java
+++ src/main/java/com/mytest/controller/Main.java
@@ -4,6 +4,7 @@ import com.mytest.mapper.TestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("/")
 public class Main {
@@ -13,5 +14,11 @@ public class Main {
     public String main() {
         System.out.println(testMapper.selectById(1).getValue());
         return "forward:首页.html";
+    }
+
+    @RequestMapping("/测试")
+    @ResponseBody()
+    public String test() {
+        return "测试啊啊啊";
     }
 }
